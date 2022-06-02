56d26f9b96080e24166cb40cb3ed399cd14c7f54
Vit <vit73123@gmail.com>
2022-06-02 10:15:01 +0300

    L01 IfElseStatementTheme 02

--- L01/IfElseStatementTheme.java
+++ L01/IfElseStatementTheme.java
@@ -23,11 +23,11 @@ public class IfElseStatementTheme {
             System.out.println("Рост не менее 1,8 м\n");
         }
         String name = "Anyname";
-        char nameFirstLetter = name.charAt(0);
+        char firstLetterName = name.charAt(0);
         System.out.println("Имя: " + name);
-        if (nameFirstLetter == 'M') {
+        if (firstLetterName == 'M') {
             System.out.println("Первая буква имени не \'М\'");
-        } else if (nameFirstLetter == 'I') {
+        } else if (firstLetterName == 'I') {
             System.out.println("Первая буква имени не \'I\'");
         } else {
             System.out.println("Подберите другое имя");
@@ -35,120 +35,109 @@ public class IfElseStatementTheme {
 
         System.out.println("\n2. Поиск max и min числа\n" +
                 "------------------------");
-        double srcNumberDbl1 = 35.4;
-        double srcNumberDbl2 = 45;
-        System.out.println("Числа: " + srcNumberDbl1 + ", " + srcNumberDbl2);
-        if (srcNumberDbl1 > srcNumberDbl2) {
-            System.out.println("Max: " + srcNumberDbl1 + "; Min - " + srcNumberDbl2);
-        } else if (srcNumberDbl1 < srcNumberDbl2) {
-            System.out.println("Max - " + srcNumberDbl2 + "; Min - " + srcNumberDbl1);
+        double number1 = 35.4;
+        double number2 = 45;
+        System.out.println("Числа: " + number1 + ", " + number2);
+        if (number1 > number2) {
+            System.out.println("Max: " + number1 + "; Min - " + number2);
+        } else if (number1 < number2) {
+            System.out.println("Max - " + number2 + "; Min - " + number1);
         }
 
         System.out.println("\n3. Работа с числом\n" +
                 "------------------");
-        int srcNumberInt = -33;
-        if (srcNumberInt == 0) {
-            System.out.println("Число " + srcNumberInt + " - ноль");
+        int number = -33;
+        if (number == 0) {
+            System.out.println("Число " + number + " - ноль");
         } else {
-            if ((srcNumberInt % 2) == 0) {
-                System.out.println("Число " + srcNumberInt + " - чётное");
+            if ((number % 2) == 0) {
+                System.out.println("Число " + number + " - чётное");
             } else {
-                System.out.println("Число " + srcNumberInt + " - нечётное");
+                System.out.println("Число " + number + " - нечётное");
             }
-            if (srcNumberInt < 0) {
+            if (number < 0) {
                 System.out.println("            и отрицательное");
+            } else {
+                System.out.println("            и положительное");
             }
         }
 
         System.out.println("\n4. Поиск одинаковых цифр в числах\n" +
                 "---------------------------------");
-        int srcNumberInt1 = 324;
-        int srcNumberInt2 = 427;
+        int srcNumber1 = 324;
+        int srcNumber2 = 427;
         int digit = 0;
-        String msgNumbers = "";
-        String msgDigits = "";
-        String msgPosition = "";
-        msgNumbers = String.valueOf(srcNumberInt1) + " и " + String.valueOf(srcNumberInt2);
-        digit = srcNumberInt1 / 100;        
-        if (digit == srcNumberInt2 / 100) {
-            msgDigits = String.valueOf(digit);
-            msgPosition = 2 + " ";
+        System.out.println("В числах " + srcNumber1 + " и " + srcNumber2);
+        digit = srcNumber1 / 100;
+        if (digit == srcNumber2 / 100) {
+            System.out.println("одинаковая цифра "+ digit + " стоит в разряде 3");
+            digit = 1;
         }
-        srcNumberInt1 %= 100;
-        srcNumberInt2 %= 100;
-        digit = srcNumberInt1 / 10;
-        if (digit == srcNumberInt2 / 10) {
-            msgDigits = String.valueOf(digit) + ", " + msgDigits;
-            msgPosition = 1 + " " + msgPosition;
+        srcNumber1 %= 100;
+        srcNumber2 %= 100;
+        digit = srcNumber1 / 10;
+        if (digit == srcNumber2 / 10) {
+            System.out.println("одинаковая цифра "+ digit + " стоит в разряде 2");
+            digit = 1;
         }
-        srcNumberInt1 %= 10;
-        srcNumberInt2 %= 10;
-        digit = srcNumberInt1;
-        if (srcNumberInt1 == srcNumberInt2) {
-            msgDigits = String.valueOf(srcNumberInt1) + " " + msgDigits;
-            msgPosition = 0 + " " + msgPosition;
+        srcNumber1 %= 10;
+        srcNumber2 %= 10;
+        if (srcNumber1 == srcNumber2) {
+            System.out.println("одинаковая цифра "+ digit + " стоит в разряде 1");
+            digit = 1;
         }
-        if (msgPosition == "") {
-            msgDigits = " одинаковых цифр нет";
-        } else {
-            msgDigits = " одинаковая(ые) цифра(ы) " + msgDigits +
-                "стоит(ят) в " + msgPosition +
-                "разряде(ах)";
+        if (digit == 0) {
+            System.out.println("одинаковых цифр нет");
         }
-        System.out.println("В числах " + msgNumbers + msgDigits);
 
         System.out.println("\n5. Определение буквы, числа или символа по их коду\n" +
                 "--------------------------------------------------");
         char srcChar = '\u0057';
-        String msgResult = "";
-        if ((srcChar > '\u0040') && (srcChar < '\u005B')) {
-            msgResult = "большая буква";
-        } else if ((srcChar > '\u0060') && (srcChar < '\u007B')) {
-            msgResult = "маленькая буква";
-        } else if ((srcChar > '\u002F') && (srcChar < '\u003A')) {
-            msgResult = "число";
+        System.out.println("Символ " + srcChar + " - ");
+        if ((srcChar >= 'A') && (srcChar <= 'Z')) {
+            System.out.println("большая буква");
+        } else if ((srcChar >= 'a') && (srcChar <= 'z')) {
+            System.out.println("маленькая буква");
+        } else if ((srcChar >= '0') && (srcChar <= '9')) {
+            System.out.println("число");
         } else {
-            msgResult = "не буква и не число";
+            System.out.println("не буква и не число");
         }
-        System.out.println("Символ " + srcChar + " - " + msgResult);
 
         System.out.println("\n6. Определение суммы вклада и начисленных банком %\n" +
                 "--------------------------------------------------");
-        double srcDeposit = 300_000.0;
-        msgResult = "Начисленный процент: ";
-        if (srcDeposit < 100_000) {
-            msgResult = msgResult + "5%\n" +
-                  "Итоговая сумма: " + String.format("%.2f", srcDeposit * 1.05);
-        } else if (srcDeposit <= 300_000) {
-            msgResult = msgResult + "7%\n" +
-                  "Итоговая сумма: " + String.format("%.2f", srcDeposit * 1.07);
+        double deposit = 300_000.0;
+        double percent;
+        if (deposit < 100_000) {
+            percent = 1.05;
+        } else if (deposit <= 300_000) {
+            percent = 1.07;
         } else {
-            msgResult = msgResult + "10%\n" +
-                  "Итоговая сумма: " + String.format("%.2f", srcDeposit * 1.10);
+            percent = 1.1;
         }
-        System.out.println("Сумма вклада: " + String.format("%.2f", srcDeposit) + '\n' +
-                msgResult);
+        System.out.println("Начисленный процент: " + String.format("%.2f", percent));
+        System.out.println("Итоговая сумма: " + String.format("%,.2f", deposit * percent));
     
         System.out.println("\n7. Определение оценки по предметам\n" +
                 "----------------------------------");
-        float pctHistory = 59;
-        float pctProgramming = 91;
+        float gradeHistory = 59;
+        float gradeProgramming = 91;
         byte markHistory = 0;
         byte markProgramming = 0;
-        if (pctHistory > 91) {
+        if (gradeHistory > 91) {
             markHistory = 5;
-        } else if (pctHistory > 73) {
+        } else if (gradeHistory > 73) {
             markHistory = 4;
-        } else if (pctHistory > 60) {
+        } else if (gradeHistory > 60) {
             markHistory = 3;
         } else {
             markHistory = 2;
         }
-        if (pctProgramming > 91) {
+        if (gradeProgramming > 91) {
             markProgramming = 5;
-        } else if (pctProgramming > 73) {
+        } else if (gradeProgramming > 73) {
             markProgramming = 4;
-        } else if (pctProgramming > 60) {
+        } else if (gradeProgramming > 60) {
             markProgramming = 3;
         } else {
             markProgramming = 2;
@@ -158,7 +147,7 @@ public class IfElseStatementTheme {
                 "Средний бал: " +
                 String.format("%.1f", ((float) (markHistory + markProgramming) / 2)) + "\n" +
                 "Средний % по предметам: " +
-                String.format("%.1f", ((pctHistory + pctProgramming) / 2)));
+                String.format("%.1f", ((gradeHistory + gradeProgramming) / 2)));
     
         System.out.println("\n8. Расчёт прибыли\n" +
                 "-----------------");
@@ -167,9 +156,9 @@ public class IfElseStatementTheme {
         double monthCost = 9_000;
         double yearProfit = (monthIncome - monthRent - monthCost) * 12;
         String yearProfitMsg;
-        if ((yearProfit) < 0) {
+        if (yearProfit < 0) {
             yearProfitMsg = String.format("%.2f", yearProfit);
-        } else if ((yearProfit) > 0) {
+        } else if (yearProfit > 0) {
             yearProfitMsg = String.format("+%.2f", yearProfit);
         } else {
             yearProfitMsg = "0.00";
@@ -178,26 +167,29 @@ public class IfElseStatementTheme {
 
         System.out.println("\n9. Определение существования треугольника\n" +
                 "-----------------------------------------");
-        double a = 3;
-        double b = 4;
-        double c = 5;
-        if ((a >= c) || (b >= c)) {
-            System.out.println("Неправильные данные. Сторона c не может быть меньше" +
-                    "одной из других сторон\n");
+        double catet1 = 3;
+        double catet2 = 4;
+        double hypotenuse = 5;
+        double temp;
+        if (hypotenuse < catet1) {
+            temp = hypotenuse;
+            hypotenuse = catet1;
+            catet1 = temp;
+        } else if (hypotenuse < catet2) {
+            temp = hypotenuse;
+            hypotenuse = catet1;
+            catet1 = temp;
         }
-        if ((a * a) + (b * b) == (c * c)) {
-            System.out.println("Гипотенуза (максимальное из чисел " +
-                    a + ", "+ b + ", "+ c + "): " + c + "\n" +
-                    "Катеты (остальные числа): " + a + ", " + b + "\n" +
-                    "Площадь треугольника равна ((a * b) / 2): " +
-                    String.format("%.1f", a * b / 2) + "\n");
+        if ((catet1 + catet2) <= hypotenuse) {
+            System.out.println("Треугольник с заданными длинами сторон не существует");
+        } else  if ((catet1 * catet1) + (catet2 * catet2) == (hypotenuse * hypotenuse)) {
+            System.out.println("Площадь треугольника равна ((a * b) / 2): " +
+                    String.format("%.1f", catet1 * catet2 / 2) + "\n");
             System.out.println("|\\" + "\n" +
                     "| \\" + "\n" +
                     "|  \\" + "\n" +
                     "|   \\" + "\n" +
                     " ___");
-        } else {
-            System.out.println("Треугольник с заданными длинами сторон не существует");
         }
 
         System.out.println("\n10. Подсчёт количества банкнот\n" +
