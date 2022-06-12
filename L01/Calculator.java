f3e56af422eab67fb86bf83ec47d5f10528cc7d5
Vit <vit73123@gmail.com>
2022-06-12 20:12:02 +0300

    L01 FinalTask 01

--- L01/Calculator.java
+++ L01/Calculator.java
@@ -1,58 +1,70 @@
 public class Calculator {
     
-    static int pow(int num, int numPow) {
-        int i;
-        int tempResult = num;
-        if (numPow > 0) {
-            for (i = numPow - 1; i > 0; i--) {
-                tempResult *= num;
-                System.out.println(tempResult);
-            }
-            System.out.println(tempResult);
-            return tempResult;          
-        } else if (numPow == 0) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
     public static void main(String[] args) {
         int num1 = 8;
         int num2 = -5;
-        int tempResult;
+        int result = 0;
         char sign = '%';
+        int error = 0;
 
         switch (sign) {
             case '+':
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + (num1 + num2));
+                result = num1 + num2;
                 break;
             case '-':
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + (num1 - num2));
+                result = num1 - num2;
                 break;
             case '*':
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + (num1 * num2));
+                result = num1 * num2;
                 break;
             case '/':
                 if (num2 == 0) {
-                    System.out.println("Ошибка: Деление на ноль");
+                    error = 1;
                     break;
                 }
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + (num1 / num2));
+                result = num1 / num2;
                 break;
             case '^':
-                tempResult = pow(num1, num2);
-                if (tempResult == 0) {
-                    System.out.println("Ошибка: Недопустимое значение степени: " + num2);
+                result = pow(num1, num2);
+                if (result == 0) {
+                    error = 2;
                     break;
                 }
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + tempResult);
                 break;
             case '%':
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + (num1 % num2));
+                result = num1 % num2;
                 break;
             default:
-                System.out.println("Ошибка: Нет операции " + sign);
+                error = 100;
         }
+        if (error == 0) {
+            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+        }
+        switch (error) {
+            case 1:
+                System.out.println("Ошибка: Деление на ноль");
+                break;
+            case 2:
+                System.out.println("Ошибка: Недопустимое значение степени: " + num2);
+                break;
+            case 100:
+                System.out.println("Ошибка: Нет операции " + sign);
+                break;
+        }
+    }
+
+    private static int pow(int num, int numPow) {
+        int i;
+        int result = num;
+        if (numPow < 0) {
+            return 0;
+        }
+        if (numPow == 0) {
+            return 1;
+        }
+        for (i = numPow - 1; i > 0; i--) {
+            result *= num;
+        }
+        return result;          
     }
 }
--- L01/GuessNumber.java
+++ L01/GuessNumber.java
@@ -1,38 +1,29 @@
 public class GuessNumber {
 
     public static void main(String[] args) {
-        final int MIN = 0 + 1;
-        final int MAX = 100;
+        final int min = 1;
+        final int max = 100;
         int num = 100;
-        int testNum = 1;
-        int testBuffer = testNum;
-        int testRange = (MAX - MIN) / 2;
+        int testNum;
+        int left = min;
+        int right = max;
         int i = 0;
 
         do {
+            testNum = (left + right) / 2;
             if (testNum > num) {
                 System.out.println("Число " + testNum +
-                        " больше того, что загадал компьютер");
-                if (testNum > testBuffer) {
-                    testRange /= 2;
-                }
-                testBuffer = testNum;
-                if ((testNum -= testRange) < MIN) {
-                    testNum = MIN;
-                }
+                        " больше того, что загадал компьютер " + 
+                        " left = " + left + " right = " + right);
+                right = testNum - 1;
             } else if (testNum < num) {
                 System.out.println("Число " + testNum +
-                        " меньше того, что загадал компьютер");
-                if (testNum < testBuffer) {
-                    testRange /= 2;
-                }
-                testBuffer = testNum;
-                if ((testNum += testRange) > MAX) {
-                    testNum = MAX;
-                }
+                        " меньше того, что загадал компьютер" +
+                        " left = " + left + " right = " + right);
+                left = testNum + 1;
             }
             i++;
-        } while (testNum != num);
+        } while ((testNum != num) && (i < 10));
         System.out.println("\nЧисло, которое загадал компьютер: " + testNum);
         System.out.println("Попыток: " + i);
     }
