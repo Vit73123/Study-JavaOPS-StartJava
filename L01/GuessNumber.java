535abbbfe8cdeb4e06bf05a6849fa8101dc0c823
Vit <vit73123@gmail.com>
2022-06-13 08:21:24 +0300

    L01 FinalTask 02

--- L01/GuessNumber.java
+++ L01/GuessNumber.java
@@ -3,28 +3,28 @@ public class GuessNumber {
     public static void main(String[] args) {
         final int min = 1;
         final int max = 100;
-        int num = 100;
-        int testNum;
+        int targetNumber = 100;
+        int playerAnswer;
         int left = min;
         int right = max;
         int i = 0;
 
         do {
-            testNum = (left + right) / 2;
-            if (testNum > num) {
-                System.out.println("Число " + testNum +
+            playerAnswer = (left + right) / 2;
+            if (playerAnswer > targetNumber) {
+                System.out.println("Число " + playerAnswer +
                         " больше того, что загадал компьютер " + 
                         " left = " + left + " right = " + right);
-                right = testNum - 1;
-            } else if (testNum < num) {
-                System.out.println("Число " + testNum +
+                right = playerAnswer - 1;
+            } else if (playerAnswer < targetNumber) {
+                System.out.println("Число " + playerAnswer +
                         " меньше того, что загадал компьютер" +
                         " left = " + left + " right = " + right);
-                left = testNum + 1;
+                left = playerAnswer + 1;
             }
             i++;
-        } while ((testNum != num) && (i < 10));
-        System.out.println("\nЧисло, которое загадал компьютер: " + testNum);
+        } while(playerAnswer != targetNumber);
+        System.out.println("\nЧисло, которое загадал компьютер: " + playerAnswer);
         System.out.println("Попыток: " + i);
     }
 }
