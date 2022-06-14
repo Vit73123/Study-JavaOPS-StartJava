22ab64dc272019a70ffc1e4766603b60c2ae4a87
Vit <vit73123@gmail.com>
2022-06-14 19:03:08 +0300

    L02 Person 00

---
+++ L02/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    char gender = 'М';
+    String name = "Сергей";
+    float height = 180F;
+    float weight = 93.5F;
+    int age = 23;
+
+    void go(double distance) {
+        System.out.println("Иду " + distance + " километров");
+    }
+
+    void sit(double time) {
+        System.out.println("Сижу " + time + " часов");
+    }
+
+    void run(double distance) {
+        System.out.println("Бегу " + distance + " километров");
+    }
+
+    void speak(String words) {
+        System.out.println("Говорю " + words);
+    }
+
+    void learnJava() {
+        System.out.println("Учу Java");
+    }
+}
