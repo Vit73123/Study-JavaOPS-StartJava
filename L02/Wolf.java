c36a14dec533a00f4cbf618e5a2ee0c42090fb4c
Vit <vit73123@gmail.com>
2022-06-17 20:33:34 +0300

    L02 Wolf 01

--- L02/Wolf.java
+++ L02/Wolf.java
@@ -8,22 +8,18 @@ public class Wolf {
     String colour;
 
     public void go() {
-        // TODO Идти
         System.out.println("Идёт");
     }
 
     public void run() {
-        // TODO Бежать
         System.out.println("Бежит");
     }
 
     public void howl() {
-        // TODO Выть
         System.out.println("Воет");
     }
 
     public void hunt() {
-        // TODO Охотиться
         System.out.println("Охотится");
     }
 }
