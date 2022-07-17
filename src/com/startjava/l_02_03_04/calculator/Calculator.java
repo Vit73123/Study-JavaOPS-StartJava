7fb5b5d81cb0649f2e112e4c03f96a32aa816c24
Vit <vit73123@gmail.com>
2022-07-17 08:13:44 +0300

    l04 02 Final HW 01 Calculator* 00

--- src/com/startjava/l_02_03_04/calculator/Calculator.java
+++ src/com/startjava/l_02_03_04/calculator/Calculator.java
@@ -2,47 +2,45 @@ package com.startjava.l_02_03_04.calculator;
 
 public class Calculator {
 
-    private int num1;
-    private int num2;
-    private char sign;
+    private static int num1;
+    private static int num2;
+    private static char sign;
+    private static boolean hasError;
 
-    public void setNum1(int num) {
-        num1 = num;
+    public static boolean hasError() {
+        return hasError;
     }
 
-    public void setNum2(int num) {
-        num2 = num;
-    }
-
-    public void setSign(char sign) {
-        this.sign = sign;
-    }
-
-    public Integer calculate(String exp) {
-        setExpressionParts(exp);
-        switch (sign) {
-            case '+':
-                return Math.addExact(num1, num2);
-            case '-':
-                return Math.subtractExact(num1, num2);
-            case '*':
-                return Math.multiplyExact(num1, num2);
-            case '/':
-                return Math.floorDiv(num1, num2);
-            case '^':
-                return (int) Math.pow(num1, num2);
-            case '%':
-                return (int) Math.floorMod(num1, num2);
-            default:
-                System.out.println("Ошибка!");
-                return null;
+    public static int calculate(String exp) {
+        hasError = false;
+        setExpression(exp);
+        int result = switch (sign) {
+            case '+' -> Math.addExact(num1, num2);
+            case '-' -> Math.subtractExact(num1, num2);
+            case '*' -> Math.multiplyExact(num1, num2);
+            case '/' -> Math.floorDiv(num1, num2);
+            case '^' -> (int) Math.pow(num1, num2);
+            case '%' -> Math.floorMod(num1, num2);
+            default -> {
+                hasError = true;
+                yield 0;
+            }
+        };
+        if (hasError) {
+            System.out.println("Ошибка в выражении!");
         }
+        return result;
     }
 
-    private void setExpressionParts(String exp) {
-        String[] expParts = exp.split(" ");
-        num1 = Integer.parseInt(expParts[0]);
-        num2 = Integer.parseInt(expParts[2]);
-        sign = expParts[1].charAt(0);
+    private static void setExpression(String exp) {
+        String[] tempExp = exp.split(" ");
+        try {
+            num1 = Integer.parseInt(tempExp[0]);
+            num2 = Integer.parseInt(tempExp[2]);
+            if (!(num1 > 0 && num2 > 0)) hasError = true;
+        } catch (NumberFormatException e) {
+            hasError = true;
+        }
+        if (!hasError) sign = tempExp[1].charAt(0);
     }
 }
--- src/com/startjava/l_02_03_04/calculator/CalculatorTest.java
+++ src/com/startjava/l_02_03_04/calculator/CalculatorTest.java
@@ -3,20 +3,22 @@ package com.startjava.l_02_03_04.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
-    
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in, "windows-1251"); // Консоль IntelliJ IDEA
-        Calculator calc = new Calculator();
+        Scanner scan = new Scanner(System.in, "windows-1251");
         String yesNo = "да";
 
-        do {
-            if (yesNo.equals("да")) {
-                System.out.print("Введите выражение: ");
-                String exp = scan.nextLine();
-                System.out.print("Результат: " + calc.calculate(exp) + '\n');
-            }
+        while (!yesNo.equals("нет")) {
+            if (!yesNo.equals("да")) continue;
+
+            System.out.print("Введите выражение: ");
+            String exp = scan.nextLine();
+            int result = Calculator.calculate(exp);
+            if (Calculator.hasError()) continue;
+
+            System.out.print("Результат: " + result + '\n');
             System.out.print("Хотите продолжить вычисления? [да/нет]: ");
             yesNo = scan.nextLine();
-        } while (!yesNo.equals("нет"));
+        }
     }
 }
