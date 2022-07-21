2e79a35db06f26f7ce729932e36f5c954c14b4ba
Vit <vit73123@gmail.com>
2022-07-21 18:59:48 +0300

    l04 02 Final HW 02 Guess 00

--- src/com/startjava/l_02_03_04/guess/GuessNumber.java
+++ src/com/startjava/l_02_03_04/guess/GuessNumber.java
@@ -3,39 +3,52 @@ package com.startjava.l_02_03_04.guess;
 import java.util.Scanner;
 
 public class GuessNumber {
-    private int min = 1;
-    private int max = 100;
+
+    private final int min = 1;
+    private final int max = 100;
     private int targetNumber;
-    private Player player1;
-    private Player player2;
+    private final int maxMoves = 10;
+    private final Player player1;
+    private final Player player2;
+
+    public int getMaxMoves() {
+        return maxMoves;
+    }
 
     public GuessNumber (Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
+        player1.setNumbers(maxMoves);
+        player2.setNumbers(maxMoves);
     }
 
     public void play() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in, "windows-1251");
+        player1.cleanNumbers();
+        player2.cleanNumbers();
 
         targetNumber = min + (int) (Math.random() * max);
-        while (!(isGuessed(player1) || isGuessed(player2)));
-
+        int currentMove = 1;
+        System.out.println("************************************************************");
+        while ((currentMove < maxMoves) && !(isGuessed(player1) || isGuessed(player2))) {
+            System.out.println("Осталось: " + (maxMoves - currentMove) + " попыток\n");
+            currentMove++;
+        }
+        player1.printNumbers();
+        player2.printNumbers();
     }
 
     private boolean isGuessed(Player player) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print(player.getName() + ", Ваше число? ");
-        player.setNumber(scan.nextInt());
-        if (player.getNumber() == targetNumber) {
-            System.out.println(player2.getName() + ", Вы угадали!");
-            return true;
-        }
-        if (player.getNumber() < targetNumber) {
-            System.out.println("Число " + player.getNumber() +
+        int number = player.giveNumber(min, max);
+        if (number < targetNumber) {
+            System.out.println("Число " + number +
                     " меньше того, что загадал компьютер");
-        } else {
-            System.out.println("Число " + player.getNumber() +
+        } else if (number > targetNumber) {
+            System.out.println("Число " + number +
                     " больше того, что загадал компьютер");
+        } else {
+            System.out.println(player.getName() + ", Вы угадали c " + player.getCurrentMove() + " попытки!");
+            return true;
         }
         return false;
     }
--- src/com/startjava/l_02_03_04/guess/Player.java
+++ src/com/startjava/l_02_03_04/guess/Player.java
@@ -1,22 +1,65 @@
 package com.startjava.l_02_03_04.guess;
 
-class Player {
-    private String name;
-    private int number;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-    public Player(String name) {
-        this.name = name;
-    }
+class Player {
+    private final String name;
+    private int[] numbers;
+    private int currentMove = 0;
 
     public String getName() {
         return name;
     }
 
-    public int getNumber() {
-        return number;
+    public int[] getNumbers() {
+        return Arrays.copyOf(numbers, currentMove);
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    public void setNumbers(int num) {
+        numbers = new int[num];
+    }
+
+    public int getCurrentMove() {
+        return currentMove;
+    }
+
+    public Player(String name) {
+        this.name = "\'" + name + "\'";
+    }
+
+    public int giveNumber(int min, int max) {
+        System.out.print(name + ", Ваше число? ");
+        while (true) {
+            try {
+                Scanner scan = new Scanner(System.in);
+                int number = scan.nextInt();
+                if ((number < min) || (number > max)) {
+                    throw new IllegalArgumentException("Недопустимое число");
+                }
+                numbers[currentMove++] = number;
+                return number;
+            } catch (InputMismatchException | IllegalArgumentException e) {
+                System.out.print(name + ", пожалуйста, введите ЦЕЛОЕ ЧИСЛО от " + min + " до " + max + ": ");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(" У игрока " + name + " превышено число попыток");
+            }
+        }
+    }
+
+    public void cleanNumbers() {
+        if (currentMove > 0) {
+            Arrays.fill(numbers, 0,  currentMove, 0);
+            currentMove = 0;
+        }
+    }
+
+    public void printNumbers() {
+        System.out.print("Числа игрока " + name + ": ");
+        for (int num : getNumbers()) {
+            System.out.printf("%3d", num);
+        }
+        System.out.println();
     }
 }
