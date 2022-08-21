a198964aec699d6ebc7fa09a6cee358570daaf51
Vit <vit73123@gmail.com>
2022-08-21 16:21:35 +0300

    l05 Databases 02 Correction

--- src/com/startjava/l_05/init_db.sql
+++ src/com/startjava/l_05/init_db.sql
@@ -1,19 +1,19 @@
 INSERT INTO jaegers
-    ( modelName,             mark,      height, weight, status,         origin,                     launch,                 kaijuKill )
+    ( modelName, mark, height, weight, status, origin, launch, kaijuKill )
 VALUES
-    ( 'Horizon Brave',      'Mark-1',   72.54,  7.890,  'Destroyed',    'China',                    'December 22, 2015',    2 ),
-    ( 'Romeo Blue',         'Mark-1',   77.72,  7.775,  'Destroyed',    'United States of America', 'December 15, 2015',    2 ),
-    ( 'Tacit Ronin',        'Mark-1',   74.37,  7.450,  'Destroyed',    'Japan',                    'December 6, 2015',     3 ),
+    ( 'Horizon Brave', 'Mark-1', 72.54, 7.890, 'Destroyed', 'China', 'December 22, 2015', 2 ),
+    ( 'Romeo Blue', 'Mark-1', 77.72, 7.775, 'Destroyed', 'United States of America', 'December 15, 2015', 2 ),
+    ( 'Tacit Ronin', 'Mark-1', 74.37, 7.450, 'Destroyed', 'Japan', 'December 6, 2015', 3 ),
 
-    ( 'Diablo Intercept',   'Mark-2',   85.34,  2.412,  'Destroyed',    'Chile',                    'October 7, 2016',      6 ),    /* 2016 */
-    ( 'Puma Real',          'Mark-2',   82.31,  2.112,  'Destroyed',    'Panama',                   'October 12, 2016',     5 ),    /* 2016 */
+    ( 'Diablo Intercept', 'Mark-2', 85.34, 2.412, 'Destroyed', 'Chile', 'October 7, 2016', 6 ), /* 2016 */
+    ( 'Puma Real', 'Mark-2', 82.31, 2.112, 'Destroyed', 'Panama', 'October 12, 2016', 5 ),    /* 2016 */
 
-    ( 'Gipsy Danger',       'Mark-3',   79.25,  1.980,  'Destroyed',    'United States of America', 'July 10, 2017',        9 ),
+    ( 'Gipsy Danger', 'Mark-3', 79.25, 1.980, 'Destroyed', 'United States of America', 'July 10, 2017', 9 ),
 
-    ( 'Striker Eureka',     'Mark-5',   76.20,  1.850,  'Destroyed',    'Australia',                'November 2, 2019',     11 ),
+    ( 'Striker Eureka', 'Mark-5', 76.20, 1.850, 'Destroyed', 'Australia', 'November 2, 2019', 11 ),
 
-    ( 'Crimson Typhoon',    'Mark-4',   76.20,  1.722,  'Destroyed',    'China',                    'August 22, 2018',      7 ),
+    ( 'Crimson Typhoon', 'Mark-4', 76.20, 1.722, 'Destroyed', 'China', 'August 22, 2018', 7 ),
 
-    ( 'Gipsy Avenger',      'Mark-6',   81.77,  2.004,  'Destroyed',    'Australia',                'January 12, 2034',     1 ),
-    ( 'Guardian Bravo',     'Mark-6',   73.21,  1.975,  'Active',       'Japan',                    'December 15, 2033',    0 ),    /* Нет данных */
-    ( 'Saber Athena',       'Mark-7',   76.91,  1.628,  'Destroyed',    'Japan',                    'December 20, 2033',    1 );
+    ( 'Gipsy Avenger', 'Mark-6', 81.77, 2.004, 'Destroyed', 'Australia', 'January 12, 2034', 1 ),
+    ( 'Guardian Bravo', 'Mark-6', 73.21, 1.975, 'Active', 'Japan', 'December 15, 2033', 0 ),    /* Нет данных */
+    ( 'Saber Athena', 'Mark-7', 76.91, 1.628, 'Destroyed', 'Japan', 'December 20, 2033', 1 );
